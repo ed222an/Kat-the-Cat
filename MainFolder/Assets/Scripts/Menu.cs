c99241aa47fbd3e50e6a0bd1dfd13590b55978ee
Emil Dannberger <ed222an@student.lnu.se>
2014-12-15 09:58:50 +0100

    Startup
    Börjat koda grundläggande rörelse för spelarobjektet.

---
+++ MainFolder/Assets/Scripts/Menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+	private string tagInstructions = "Instructions";
+	private GameObject instructions;
+
+	// Use this for initialization
+	void Start ()
+	{
+		instructions = GameObject.FindGameObjectWithTag (tagInstructions);
+		instructions.active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadNextLevel()
+	{
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+	public void ShowInstructions()
+	{
+		instructions.active = true;
+	}
+
+	public void CloseInstructions()
+	{
+		instructions.active = false;
+	}
+}
---
+++ MainFolder/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float maxSpeed = 10f;
+	public bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		float move = Input.GetAxis ("Horizontal");
+		rigidbody.velocity = new Vector3 (move * maxSpeed, rigidbody.velocity.y, 0.0f);
+
+		if (move > 0 && !facingRight)
+		{
+			Flip();
+		}
+		else if(move < 0 && facingRight)
+		{
+			Flip();
+		}
+	}
+
+	// Flips the world around the player, allowing us to only use 1 set of animations.
+	private void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
