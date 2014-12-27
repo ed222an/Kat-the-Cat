5ce9e22a5f6e531d7810cb485c9d290c1bc092a6
Emil Dannberger <ed222an@student.lnu.se>
2014-12-27 15:19:07 +0100

    Basic character movement & camera follow
    Added left & right movement aswell as the ability to jump. Also made the
    camera follow the player in a smooth way.

--- MainFolder/Assets/Scripts/PlayerMovement.cs
+++ MainFolder/Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,51 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
+	// Speed & animaton related
 	public float maxSpeed = 10f;
 	public bool facingRight = true;
+	private Animator anim;
+
+	// Ground stuff for jumping
+	public bool grounded = false;
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+	private float groundRadius = 0.2f;
+	private bool doubleJump = false;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();	
 	}
-	
-	// Update is called once per frame
+
 	void FixedUpdate ()
 	{
+		// Check if the player is grounded
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+
+		if(grounded)
+		{
+			doubleJump = false;
+		}
+
+		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+
+		/*
+		// Disables movement while jumping
+		if(!grounded)
+		{
+			return;
+		}
+		*/
+
 		float move = Input.GetAxis ("Horizontal");
-		rigidbody.velocity = new Vector3 (move * maxSpeed, rigidbody.velocity.y, 0.0f);
+
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+
+		rigidbody2D.velocity = new Vector3 (move * maxSpeed, rigidbody2D.velocity.y, 0.0f);
 
 		if (move > 0 && !facingRight)
 		{
@@ -24,6 +56,20 @@ public class PlayerMovement : MonoBehaviour
 		else if(move < 0 && facingRight)
 		{
 			Flip();
+		}
+	}
+
+	void Update()
+	{
+		if((grounded || !doubleJump) && Input.GetKeyDown(KeyCode.Space))
+		{
+			anim.SetBool ("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0,jumpForce));
+
+			if(!doubleJump && !grounded)
+			{
+				doubleJump = true;
+			}
 		}
 	}
 
